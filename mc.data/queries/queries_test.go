32addeac1d982c24b6a4da3e57bad795f6b2e023
Drew Coogan <andrew.l.coogan@gmail.com>
2026-02-12 15:47:32 -0800

    Update queries_test.go
    cleaned up the test a bit, added a fallback raw count of files and made sure those agree. we dont want any files in there that are not assigned or allocated.

--- mc.data/queries/queries_test.go
+++ mc.data/queries/queries_test.go
@@ -1,25 +1,51 @@
 package queries
 
 import (
+	"io/fs"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func TestQueryHelperAllStringsRecursive(t *testing.T) {
+	// collect all query paths in QueryHelper
 	var paths []string
 	collectQueryPaths(reflect.ValueOf(QueryHelper), &paths)
 
 	if len(paths) == 0 {
-		t.Fatal("collectQueryPaths found no query paths in QueryHelper")
+		t.Fatal("no query paths in QueryHelper found")
 	}
 
 	for _, path := range paths {
 		t.Run(path, func(t *testing.T) {
-			content := Get(path)
-			if content == "" {
-				t.Errorf("query file %q is empty or missing", path)
+			if content := Get(path); content == "" {
+				t.Errorf("query file %q is empty", path)
 			}
 		})
+	}
+
+	// raw count of .sql files in queries folder
+	root := "../queries"
+	count := 0
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && strings.HasSuffix(strings.ToLower(d.Name()), ".sql") {
+			count++
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Fatalf("Error walking the path: %v", err)
+	}
+
+	if count != len(paths) {
+		t.Fatalf("number of .sql files in %s does not match number of query paths in QueryHelper (%d != %d)", root, count, len(paths))
 	}
 }
 
@@ -27,28 +53,16 @@ func TestQueryHelperAllStringsRecursive(t *testing.T) {
 // appends every string field value to paths. Used to find all query file paths
 // in QueryHelper and its substructure.
 func collectQueryPaths(v reflect.Value, paths *[]string) {
-	if v.Kind() == reflect.Pointer {
-		if v.IsNil() {
-			return
-		}
-		v = v.Elem()
-	}
-	if v.Kind() != reflect.Struct {
-		return
-	}
+	// .NumFields() will bomb if it doesnt get a struct, but we know it will be a struct
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
-		switch field.Kind() {
-		case reflect.String:
+
+		if field.Kind() == reflect.String {
 			if s := field.String(); s != "" {
 				*paths = append(*paths, s)
 			}
-		case reflect.Struct:
+		} else {
 			collectQueryPaths(field, paths)
-		case reflect.Pointer:
-			if field.Elem().Kind() == reflect.Struct {
-				collectQueryPaths(field, paths)
-			}
 		}
 	}
 }
